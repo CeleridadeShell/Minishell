1fa3b739a586036a305b52bab6750e47ee808ab9
Christian Camargo Del Moro <christian.delmor@gmail.com>
2023-05-01 17:28:39 -0300

    Exec pipes funcionando parcialmente.

--- src/execution/token_execution.c
+++ src/execution/token_execution.c
@@ -6,7 +6,7 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:17:44 by ccamargo          #+#    #+#             */
-/*   Updated: 2023/04/26 22:40:32 by ccamargo         ###   ########.fr       */
+/*   Updated: 2023/05/01 17:26:38 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,18 @@ static char	*form_tested_path(t_token *token,char *path)
 
 static void	run_path(t_shell *shell, t_token *token)
 {
-	int		child_pid;
+//	int		child_pid;
 	int		i;
 
 	i = 0;
 	if (!access(token->cmd[0], F_OK))
 	{
-		child_pid = fork();
+/* 		child_pid = fork();
 		if (child_pid == 0)
-		{
+		{ */
 			execve(token->cmd[0], token->cmd, shell->envp);
-		}
-		waitpid(child_pid, NULL, 0);
+/* 		}
+		waitpid(child_pid, NULL, 0); */
 	}
 	else
 	{
@@ -53,7 +53,7 @@ static void	run_path(t_shell *shell, t_token *token)
 static void	run_sys_bin(t_shell *shell, t_token *token)
 {
 	char	*tested_path;
-	int		child_pid;
+//	int		child_pid;
 	int		i;
 
 	i = 0;
@@ -62,12 +62,12 @@ static void	run_sys_bin(t_shell *shell, t_token *token)
 		tested_path = form_tested_path(token, shell->paths[i]);
 		if (!access(tested_path, F_OK))
 		{
-			child_pid = fork();
+/* 			child_pid = fork();
 			if (child_pid == 0)
-			{
+			{ */
 				execve(tested_path, token->cmd, shell->envp);
-			}
-			waitpid(child_pid, NULL, 0);
+/* 			}
+			waitpid(child_pid, NULL, 0); */
 			break ;
 		}
 		ft_freethis(&tested_path, NULL);
@@ -81,13 +81,77 @@ static void	run_sys_bin(t_shell *shell, t_token *token)
 void	execute_token(t_shell *shell)
 {
 	t_token	*token;
+	int		fd[2];
+	int		fd_original[2];
+ 	pid_t	pid;
+ 	int		fd_in = 0;
+	int		cmd_i;
 
 	token = shell->h_token;
-	if (check_built_in(token, shell) != 0)
+	cmd_i = 0;
+	fd_original[0] = dup(STDIN_FILENO);
+	fd_original[1] = dup(STDOUT_FILENO);
+	while (token)
+    {
+		if (token->type == WORD)
+		{
+			pipe(fd);
+			if (fd_in != 0)
+			{
+				if (dup2(fd_in, STDIN_FILENO) == -1)
+				{
+					printf("Deu bosta!\n");
+				}
+			}
+			if (token->next_token == NULL)
+			{
+				if (dup2(fd_original[1], STDOUT_FILENO) == -1)
+				{
+					printf("Deu bosta!\n");
+				}
+			}
+			else
+			{
+				if (dup2(fd[1], STDOUT_FILENO) == -1)
+				{
+					printf("Deu bosta!\n");
+				}
+			}
+			if (check_built_in(token, shell) != 0)
+			{
+				pid = fork();
+				if (pid == -1)
+					exit(EXIT_FAILURE);
+				else if (pid == 0)
+				{
+					close(fd[0]);
+					if (!ft_strncmp(token->cmd[0], ".", 1) || !ft_strncmp(token->cmd[0], "/", 1))
+						run_path(shell, token);
+					else
+						run_sys_bin(shell, token);
+					exit(EXIT_FAILURE);
+				}
+				else
+				{
+					wait(NULL);
+					if (fd_in != 0)
+					{
+						close(fd_in);
+					}
+					close(fd[1]);
+					fd_in = fd[0];
+					cmd_i++;
+				}
+			}
+		}
+		token = token->next_token;
+    }
+	if (dup2(fd_original[0], STDIN_FILENO) == -1)
 	{
-		if (!ft_strncmp(token->cmd[0], ".", 1) || !ft_strncmp(token->cmd[0], "/", 1))
-			run_path(shell, token);
-		else
-			run_sys_bin(shell, token);
+		printf("Deu bosta!\n");
 	}
+	/* if (token->n_cmds == cmd_i)
+	{
+		dup2(fd_original[0], STDIN_FILENO);
+	} */
 }
